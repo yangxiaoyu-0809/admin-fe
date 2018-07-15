5ea4b0e9fb2ff91384e7e8ac20da939ce6114fad
yangxiaoyu <1150055925@qq.com>
2018-07-15 11:06:42 +0800

    通用部分的开发

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {BrowserRouter as Router,Switch,Redirect,Route,Link} from 'react-router-dom';
 
-import 'font-awesome/css/font-awesome.min.css'
-import './index.css';
-import './index.scss';
+import Layout from 'component/layout/index.jsx';
+//页面
+import Home from 'page/home/index.jsx';
 
+class App extends React.Component{
+	render(){
+		return (
+			<Router>
+				<Layout>
+					<Switch>
+						<Route exact path='/' component={Home}/>
+						<Route path='/product' component={Home}/>
+						<Route path='/product-category' component={Home}/>
+					</Switch>
+				</Layout>
+			</Router>
+		);
+	}
+}
 ReactDom.render(
-	<div>
-		<h1>Hello,world!</h1>,
-	</div>,
+	<App />,
 	document.getElementById('app')
 );
